8f8679d9e58607013a2c81887bb9500d3f357a35
Maurice Faber <morriz@idiotz.nl>
2017-11-08 03:34:10 +0100

    - added prometheus metrics to api too - refactored and cleaned up old config

--- bin/install.sh
+++ bin/install.sh
@@ -54,36 +54,37 @@ until ks rollout status -w deployment/kube-dns > /dev/null 2>&1; do sleep 1; pri
 #printf "${COLOR_BLUE}Applying RBAC for minikube asap\n${COLOR_GREEN}"
 #k apply -f $root/k8s/minikube-rbac.yaml
 
-printf "\n${COLOR_BLUE}Claiming persistent volume\n${COLOR_GREEN}"
+printf "\n${COLOR_BLUE}[CLUSTER] Installing namespaces${COLOR_GREEN}\n"
+k create namespace "dev" > /dev/null 2>&1
+k create namespace "monitoring" > /dev/null 2>&1
+k create namespace "drone" > /dev/null 2>&1
+
+printf "${COLOR_PURPLE}[CLUSTER] Waiting for namespaces to become available"
+until k get namespace dev monitoring drone > /dev/null 2>&1; do sleep 1; printf "."; done
+
+printf "\n${COLOR_BLUE}[CLUSTER] Creating persistent volume\n${COLOR_GREEN}"
 k apply -f $root/k8s/pvc.yaml
 
-printf "${COLOR_BLUE}Deploying registry cache chart first\n${COLOR_GREEN}"
-helm template -n mostack-cache $root/charts/docker-registry -f $root/values/docker-registry-cache.yaml | ks apply -f -
-[ $? -ne 0 ] && printf "${COLOR_LIGHT_RED}Something went wrong installing registry cache chart${COLOR_NC}\n" && exit 1
+printf "${COLOR_BLUE}[KUBE-SYSTEM] Deploying Docker Registry cache first${COLOR_GREEN}\n"
+helm template -n system-cache $root/charts/docker-registry -f $root/values/docker-registry-cache.yaml | ks apply -f -
+[ $? -ne 0 ] && printf "${COLOR_LIGHT_RED}Something went wrong installing Docker Registry cache${COLOR_NC}\n" && exit 1
 
-printf "${COLOR_PURPLE}Waiting for registry cache to become available${COLOR_BROWN}\n"
-ks rollout status -w deployment/mostack-cache-docker-registry
+printf "${COLOR_PURPLE}[KUBE-SYSTEM] Waiting for Docker Registry cache to become available${COLOR_BROWN}\n"
+ks rollout status -w deployment/system-cache-docker-registry
 
 #printf "${COLOR_BLUE}deploying nginx controller\n${COLOR_GREEN}"
-#helm template -n mostack $root/charts/nginx-ingress -f $root/values/nginx-ingress.yaml | k apply -f -
+#helm template -n system $root/charts/nginx-ingress -f $root/values/nginx-ingress.yaml | k apply -f -
 #[ $? -ne 0 ] && printf "${COLOR_LIGHT_RED}Something went wrong installing nginx controller${COLOR_NC}\n" && exit 1
 #printf "${COLOR_NC}"
 #
 #printf "${COLOR_BLUE}waiting for nginx controller to become available${COLOR_BROWN}\n"
-#k rollout status -w deployment/mostack-nginx-ingress-controller
+#k rollout status -w deployment/system-nginx-ingress-controller
 #printf "${COLOR_NC}"
 
-printf "${COLOR_BLUE}Installing namespaces 'monitoring' and 'drone'${COLOR_GREEN}\n"
-k create namespace "monitoring" > /dev/null 2>&1
-k create namespace "drone" > /dev/null 2>&1
+printf "${COLOR_BLUE}[MONITORING] Installing Prometheus Operator${COLOR_GREEN}\n"
+helm template -n system $root/charts/prometheus-operator -f $root/values/prometheus-operator.yaml | km apply -f -
 
-printf "${COLOR_PURPLE}Waiting for namespaces 'monitoring' and 'drone' to become available"
-until k get namespace monitoring drone > /dev/null 2>&1; do sleep 1; printf "."; done
-
-printf "\n${COLOR_BLUE}Installing Prometheus Operator${COLOR_GREEN}\n"
-helm template -n mostack $root/charts/prometheus-operator -f $root/values/prometheus-operator.yaml | km apply -f -
-
-printf "${COLOR_PURPLE}Waiting for Prometheus Operator to register custom resource definitions"
+printf "${COLOR_PURPLE}[MONITORING] Waiting for Prometheus Operator to register custom resource definitions"
 until km get customresourcedefinitions servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
 until km get customresourcedefinitions prometheuses.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
 until km get customresourcedefinitions alertmanagers.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
@@ -91,22 +92,34 @@ until km get servicemonitors.monitoring.coreos.com > /dev/null 2>&1; do sleep 1;
 until km get prometheuses.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
 until km get alertmanagers.monitoring.coreos.com > /dev/null 2>&1; do sleep 1; printf "."; done
 
-printf "\n${COLOR_BLUE}Installing Kube Prometheus${COLOR_GREEN}\n"
-helm template -n mostack $root/charts/kube-prometheus -f $root/values/kube-prometheus.yaml | km apply -f -
+printf "\n${COLOR_BLUE}[KUBE_SYSTEM] Installing Kube exporters for Prometheus consumption${COLOR_GREEN}\n"
+helm template -n system $root/charts/kube-prometheus -f $root/values/kube-prometheus.yaml --set onlyServices=true | k apply -f -
 
-printf "${COLOR_BLUE}Deploying remaining (independent) charts${COLOR_GREEN}\n"
-helm template -n mostack $root/charts/grafana -f $root/values/grafana.yaml | km apply -f -
-helm template -n mostack $root/charts/kube-lego -f $root/values/kube-lego.yaml | ks apply -f -
-helm template -n mostack $root/charts/docker-registry -f $root/values/docker-registry.yaml | ks apply -f -
-helm template -n mostack $root/charts/drone -f $root/values/drone.yaml | kd apply -f -
-helm template -n mostack $root/charts/api -f $root/values/api.yaml | k apply -f -
+printf "${COLOR_BLUE}[MONITORING] Installing Prometheus, Alertmanager and Grafana${COLOR_GREEN}\n"
+helm template -n system $root/charts/prometheus -f $root/values/prometheus.yaml | k apply -f -
+helm template -n team-frontend $root/charts/prometheus -f $root/values/prometheus-team-frontend.yaml | k apply -f -
+helm template -n system $root/charts/alertmanager -f $root/values/alertmanager.yaml | k apply -f -
+helm template -n team-frontend $root/charts/alertmanager -f $root/values/alertmanager-team-frontend.yaml | k apply -f -
+helm template -n system $root/charts/grafana -f $root/values/grafana.yaml | km apply -f -
+
+printf "${COLOR_BLUE}[KUBE-SYSTEM] Deploying Kube Lego${COLOR_GREEN}\n"
+helm template -n system $root/charts/kube-lego -f $root/values/kube-lego.yaml | ks apply -f -
+
+printf "${COLOR_BLUE}[KUBE-SYSTEM] Deploying Docker Registry${COLOR_GREEN}\n"
+helm template -n system $root/charts/docker-registry -f $root/values/docker-registry.yaml | ks apply -f -
+
+printf "${COLOR_BLUE}[DRONE] Deploying Drone${COLOR_GREEN}\n"
+helm template -n system $root/charts/drone -f $root/values/drone.yaml | kd apply -f -
+
+printf "${COLOR_BLUE}[DEFAULT] Deploying API${COLOR_GREEN}\n"
+helm template -n team-frontend $root/charts/api -f $root/values/api.yaml | k apply -f -
 [ $? -ne 0 ] && printf "${COLOR_LIGHT_RED}Something went wrong installing app charts${COLOR_NC}\n" && exit 1
 
-printf "${COLOR_BLUE}Deploying ELK stack\n${COLOR_GREEN}"
+printf "${COLOR_BLUE}[LOGGING] Deploying ELK stack\n${COLOR_GREEN}"
 k apply -f $root/k8s/elk/
 
-printf "${COLOR_PURPLE}Waiting for kube-lego to become available${COLOR_BROWN}\n"
-ks rollout status -w deployment/mostack-kube-lego
+printf "${COLOR_PURPLE}[KUBE-SYSTEM] Waiting for kube-lego to become available${COLOR_BROWN}\n"
+ks rollout status -w deployment/system-kube-lego
 
 printf "${COLOR_BLUE}Starting tunnels${COLOR_NC}\n"
 # and run tunnel to minikube node's port 80 and 443
