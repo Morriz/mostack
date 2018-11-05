73c2a5902d3ce532ca3f640dcdb46c34da840659
Maurice Faber <morriz@idiotz.nl>
2018-11-06 00:50:38 +0100

    before good night

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,11 +4,10 @@ root=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)
 shopt -s expand_aliases
 . bin/aliases
 
-printf "${COLOR_BLUE}Deploying Flux Operator${COLOR_NC}\n"
-hs flux \ 
+# helm --tiller-namespace=kube-system --namespace=system install --name flux \ 
+hs flux \
 --set rbac.create=true \
 --set helmOperator.create=true \
 --set git.url=ssh://git@github.com:Morriz/mostack \
---set git.branch=gitops
---namespace system \
+--set git.branch=gitops \
 charts/flux
