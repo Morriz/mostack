ea719267da2d3ea9c15adf286d0c20f31fc0a5cf
Maurice Faber <morriz@idiotz.nl>
2019-04-02 05:02:46 +0200

    cleaned up: 2

--- bin/ngrok.sh
+++ bin/ngrok.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-root=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+root=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)
 . $root/bin/colors.sh
 shopt -s expand_aliases
 . $root/bin/aliases
 . $root/secrets/local.sh
 
 # uses 'mo'
-which mo > /dev/null
+which mo >/dev/null
 if [ $? -ne 0 ]; then
   echo "'mo' needs to be installed:"
   echo "curl https://raw.githubusercontent.com/tests-always-included/mo/master/mo -o mo&& chmod u+x mo && mv mo /usr/local/bin)"
@@ -14,5 +14,5 @@ if [ $? -ne 0 ]; then
 fi
 
 killall ngrok
-cat $root/templates/ngrok.sh | mo > /tmp/ngrok.yaml
-ngrok start --log-level "info" -config=/tmp/ngrok.yaml dev dev-tls &
+cat $root/templates/ngrok.yaml | mo >/tmp/ngrok.yaml
+ngrok start --log-level "debug" -config=/tmp/ngrok.yaml dev dev-tls &
