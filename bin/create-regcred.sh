255cb01402a5beeabec3d2863f17718beb335bee
Maurice Faber <morriz@idiotz.nl>
2019-04-14 11:50:18 +0200

    added regcreds

--- bin/create-regcred.sh
+++ bin/create-regcred.sh
@@ -1,8 +1,11 @@
+. ./.env.sh
+. secrets/${PROVIDER}.sh
+
 ns=prod
 service=api
 # create a deploy token @ your git repo and insert user+pass:
-user=gitlab+deploy-token-26305
-pass=
+user=$REG_USER
+pass=$REG_PASS
 
 k="kubectl -n $ns"
 $k create secret docker-registry regcred-$service --docker-server=registry.gitlab.com --docker-email=devops@yourdoma.in \
