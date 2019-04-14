7feea26eefc394ca9077e1988e364f07bce7c16f
Maurice Faber <morriz@idiotz.nl>
2019-04-14 12:37:17 +0200

    updated regcred hosts

--- bin/create-regcred.sh
+++ bin/create-regcred.sh
@@ -1,14 +1,14 @@
 . ./.env.sh
 . secrets/${PROVIDER}.sh
 
-ns=system
+ns=$1
 service=api
 # create a deploy token @ your git repo and insert user+pass:
 user=$REG_USER
 pass=$REG_PASS
 
 k="kubectl -n $ns"
-$k create secret docker-registry regcred-$service --docker-server=registry.gitlab.com --docker-email=devops@yourdoma.in \
+$k create secret docker-registry regcred-$service --docker-server=$REGISTRY_HOST --docker-email=devops@yourdoma.in \
     --docker-username=$user \
     --docker-password=$pass \
     --dry-run -o yaml >./releases/$ns/${service}-regcred.yaml
